8d3b6be67608b5607cbaef736c3672d6182a1593
Chirayu Chiripal <chirayu.chiripal@gmail.com>
2014-08-07 01:06:52 +0530

    fix table name in setup query
    Signed-off-by: Chirayu Chiripal <chirayu.chiripal@gmail.com>

--- database_installation_import_files/import_these_into_mysql_in_given_order/import_at_last/1_init_privileges.sql
+++ database_installation_import_files/import_these_into_mysql_in_given_order/import_at_last/1_init_privileges.sql
@@ -7,7 +7,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 
-INSERT INTO `privilege_master` (`privilege_id`, `privilege_name`, `faculty_master_access`, `inst_master_access`, `prog_master_access`, `dept_master_access`, `academic_calendar_access`, `attendance_master_access`, `student_master_access`, `sub_master_access`, `user_master_access`, `privilege_master_access`, `offers_master_access`, `syllabus_access`, `teaches_access`, `reports`) VALUES
+INSERT INTO `Privilege_Master` (`privilege_id`, `privilege_name`, `faculty_master_access`, `inst_master_access`, `prog_master_access`, `dept_master_access`, `academic_calendar_access`, `attendance_master_access`, `student_master_access`, `sub_master_access`, `user_master_access`, `privilege_master_access`, `offers_master_access`, `syllabus_access`, `teaches_access`, `reports`) VALUES
 (1, 'admin', '111', '111', '111', '111', '111', '111', '111', '111', '111', '111', '111', '111', '111', '1');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- database_installation_import_files/import_these_into_mysql_in_given_order/import_at_last/2_default_accounts.sql
+++ database_installation_import_files/import_these_into_mysql_in_given_order/import_at_last/2_default_accounts.sql
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- Dumping data for table `user_master`
 --
 
-INSERT INTO `user_master` (`user_name`, `email_id`, `enc_algo`, `salt_type`, `salt`, `user_password`, `user_status`, `user_creation_date`, `user_update_date`, `faculty_id`, `privilege_id`) VALUES
+INSERT INTO `User_Master` (`user_name`, `email_id`, `enc_algo`, `salt_type`, `salt`, `user_password`, `user_status`, `user_creation_date`, `user_update_date`, `faculty_id`, `privilege_id`) VALUES
 ('admin', 'admin@salam.com', 1, 1, '1024823780535d64ce7fd332.53283497', '9eb62f5887240a9bd354d1a8a5d7d5ff863158184282c655c5334b7cf500e40b81cd46f7c6d808fb13b03c2bb04bfba0b55896f9c98931fab39cb2bd34b5c10e', 1, '2014-04-28 01:43:02', '2014-04-27 20:13:02', NULL, 1),
 ('developer', 'chirayu.chiripal@gmail.com', 1, 1, '5502270520611a146ed63.60627028', '5bfa6548ed74bfe9bb8396e52d10222ca274eb8376a2b24d323a7a5a7cd5c368f1f1c2163cca172a6f836d727c7a77fa4e7ced9ff0a53caf120071f15604ed75', 0, '2013-09-22 00:00:00', '2014-02-07 11:32:25', NULL, 1);
 
