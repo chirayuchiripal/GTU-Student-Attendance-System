7eaad5afeea51fdb3e239b446522f6e280669bb7
Chirayu Chiripal <chirayu.chiripal@gmail.com>
2014-08-08 14:22:46 +0530

    change default config
    Signed-off-by: Chirayu Chiripal <chirayu.chiripal@gmail.com>

--- core/config/db/credentials.db.sample.php
+++ core/config/db/credentials.db.sample.php
@@ -2,9 +2,9 @@
 return array(
 // Refer Zend Framework 2 Reference Manual for Valid Database Drivers & other Options
 'driver'	=> 'Pdo_Mysql',
-'database'	=> 'ams_v0.3',
-'username'	=> 'chirayu',
-'password'	=> 'chirayu',
+'database'	=> 'salam',
+'username'	=> 'salam',
+'password'	=> 'salam',
 'hostname'	=> '127.0.0.1',
 );
 ?>
