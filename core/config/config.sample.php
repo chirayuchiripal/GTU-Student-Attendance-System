ec2c0a8f6937ed4794e0665b8bbcb2e6ed346a61
Chirayu Chiripal <chirayu.chiripal@gmail.com>
2014-08-08 14:28:10 +0530

    add installation note in sample config files
    Signed-off-by: Chirayu Chiripal <chirayu.chiripal@gmail.com>

--- core/config/config.sample.php
+++ core/config/config.sample.php
@@ -1,8 +1,11 @@
 <?php
+/* Note: Create a copy of this file with following name
+   "config.php" (without quotes) and change your configuration.
+ */
 /* If hosted on root like 127.0.0.1/ then
    APP_NAME should be '/'
    Or else hosted on some directory like 127.0.0.1/salam/ then
    APP_NAME should be '/salam/'
-*/
+ */
 define('APP_NAME', '/');
 ?>
--- core/config/db/credentials.db.sample.php
+++ core/config/db/credentials.db.sample.php
@@ -1,4 +1,7 @@
 <?php
+/* Note: Create a copy of this file with following name
+   "credentials.db.php" (without quotes) and change your configuration.
+ */
 return array(
 // Refer Zend Framework 2 Reference Manual for Valid Database Drivers & other Options
 'driver'	=> 'Pdo_Mysql',
