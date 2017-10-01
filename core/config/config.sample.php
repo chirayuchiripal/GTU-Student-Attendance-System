b8c8044b1ff7f754b41266bb0de59734d9d4d7e4
Chirayu Chiripal <D-storm@users.noreply.github.com>
2017-10-01 19:41:19 +0530

    Remove forward slash from APP_NAME

--- core/config/config.sample.php
+++ core/config/config.sample.php
@@ -3,9 +3,9 @@
    "config.php" (without quotes) and change your configuration.
  */
 /* If hosted on root like 127.0.0.1/ then
-   APP_NAME should be '/'
+   APP_NAME should be ''
    Or else hosted on some directory like 127.0.0.1/salam/ then
-   APP_NAME should be '/salam/'
+   APP_NAME should be '/salam'
  */
-define('APP_NAME', '/');
+define('APP_NAME', '');
 ?>
